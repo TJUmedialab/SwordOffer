f557bb9faacae1abcfd424addf8503fb5291162e
zhyf <basaltzhang@gmail.com>
2014-07-10 11:59:36 +0800

    example 7 is applied

--- SwardOffer7/main.cpp
+++ SwardOffer7/main.cpp
@@ -7,9 +7,6 @@ template <typename T>
 class CQueue
 {
 public:
-	CQueue();
-	~CQueue();
-
 	void appendTail(const T& node);
 	T deleteHead();
 private:
@@ -17,3 +14,39 @@ private:
 	stack<T> stack2;
 };
 
+template <typename T>
+void CQueue<T>::appendTail(const T& node)
+{
+	stack1.push(node);
+}
+
+template <typename T>
+T CQueue<T>::deleteHead()
+{
+	T temp;
+	if ( stack2.empty() ) {
+		while ( !stack1.empty() ) {
+			temp = stack1.top();
+			stack2.push(temp);
+			stack1.pop();
+		}
+	}
+	if ( stack2.empty() ) {
+		cerr << "the list is empty, cannot out list\n";
+		exit(0);
+	}
+	temp = stack2.top();
+	stack2.pop();
+	return temp;
+}
+int main(int argc, char* argv[])
+{
+	CQueue<int> m_queue;
+	for (int i = 0; i < 10; i ++) {
+		m_queue.appendTail(i);
+	}
+	for (int i = 0; i < 10; i ++) {
+		cout << m_queue.deleteHead() << endl;
+	}
+	return 0;
+}
